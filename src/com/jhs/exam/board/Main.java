eeb954d429762302f7518aa6ce5fe0ea04a8d719
Hyuns610 <hunsik33@naver.com>
2023-02-07 19:45:05 +0900

    명령ㄹ어명령어 입력받기

--- src/com/jhs/exam/board/Main.java
+++ src/com/jhs/exam/board/Main.java
@@ -1,9 +1,17 @@
 package com.jhs.exam.board;
+
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
     System.out.println("== 게시판 v 0.1 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.println("명령)");
+    String cmd = sc.nextLine();
+    System.out.println("입력 된 명령어 : " + cmd);
     System.out.println("== 프로그램 종료 ==");
+
+    sc.close();
   }
 }
